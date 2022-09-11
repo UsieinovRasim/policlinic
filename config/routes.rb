13a0605f6c321d639e5535fed70a25739fcc48be
Усеинов Расим <91624311+UsieinovRasim@users.noreply.github.com>
2022-09-11 19:39:50 +0300

    amending

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "admins#index", as: "home"
+
+  resources :admins
+  resources :doctors
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+end
